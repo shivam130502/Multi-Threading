6c28309604abe895918cf160918ced0025f029d7
Shivam Dhir <shivamdhir90@gmail.com>
2024-09-14 19:46:45 -0700

    created classes A and B to practice multithreading

--- src/App.java
+++ src/App.java
@@ -1,5 +1,28 @@
+class A extends Thread {
+
+    public void run(){
+        System.err.println("Class A");
+    }
+
+}
+
+class B extends Thread{
+
+    public void run(){
+        System.out.println("Class B");
+    }
+
+}
+
 public class App {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Thread obj1 = new A();
+        Thread obj2 = new B();
+
+        obj1.run();
+        obj2.run();
+
     }
 }
